6f60aee9701e213b4df490836d1c056516c6374b
Marcus August Christiansen <s175185@student.dtu.dk>
2019-12-20 11:24:26 +0100

    Initial Commit

---
+++ StringR.IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StringR.IOS
+//
+//  Created by Marcus Christiansen on 20/12/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StringR.IOSTests/StringR_IOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  StringR_IOSTests.swift
+//  StringR.IOSTests
+//
+//  Created by Marcus Christiansen on 20/12/2019.
+//  Copyright © 2019 StringR. All rights reserved.
+//
+
+import XCTest
+@testable import StringR_IOS
+
+class StringR_IOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
